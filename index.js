385d28b099673ae740495db9ed68e8d68eb905ad
m <139261114+mauriciorfdev@users.noreply.github.com>
2024-04-24 19:26:34 -0400

    1) POST request - check isAdmin at body. 2) logger middleware. 3) Routes for GET request at '/user/:id' path, using middleware stack/sub-stack

--- index.js
+++ index.js
@@ -3,13 +3,39 @@ const app = express()
 const port = 3000
 
 app.use( express.json() )
+app.use( express.urlencoded({extended:false}))
+//app.use( express.urlencoded({extended:false}))
 
+const logger = function(req, res, next){
+    console.log('Logger - METHOD: '+req.method);
+    next()
+}
+
+
+
+app.use(logger)
+
+
+app.get('/user/:id', (req, res, next)=>{
+    console.log('ID: ', req.params.id)
+    if(req.params.id == 0) next('route')
+    else next()
+}, (req, res, next)=>{
+    res.send('Regular')
+})
+app.get('/user/:id', (req, res, next)=>{
+    res.send('Special')
+})
+
+
+//GET - POST TO HOME('/') PATH
 app.get('/', (req,res)=>{
-    res.send('GET request')
+    res.send(`GET request`)
 })
 app.post('/', (req,res)=>{
-    res.send('POST request')
+    res.send(`POST Request - isAdmin:${req.body.isAdmin}`)
 })
+
 
 app.listen(port, ()=>{
     console.log(`LISTEN ON PORT ${port}...`);
